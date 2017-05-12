215f887c98b82642eb37ce054dc4c24d233b5496
Jorge Ccochayhua <jorge_ccochayhua@usmp.pe>
2017-05-12 17:39:33 -0500

    Se agregó cabmiois en el appconfig y se comprobo que la clase Residente si sale en la base de datos

---
+++ Condominio.Entities/Residente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condominio.Entities
+{
+    public class Residente
+    {
+        public int ResidenteId { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public int Dni { get; set; }
+        public int Telefono { get; set; }
+
+    }
+}
--- Condominio.Persistence/CondominioDbContext.cs
+++ Condominio.Persistence/CondominioDbContext.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using Condominio.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Condominio.Persistence
 {
-    class Program
+    public class CondominioDbContext : DbContext
     {
+        public DbSet<Residente> Residentes { get; set; }
+
         static void Main(string[] args)
         {
+
         }
     }
+
+    
 }
