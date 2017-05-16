402ae1237172f1455393923bfe2aa146a9bc683b
Jorge Ccochayhua <jorge_ccochayhua@usmp.pe>
2017-05-15 20:27:36 -0500

    Agregando los entitiesConfiguration

---
+++ Condominio.Entities/CondominioDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Condominio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condominio.Persistence
+{
+    public class CondominioDbContext : DbContext
+    {
+        public DbSet<Residente> Residentes { get; set; }
+        
+    }
+
+    
+}
